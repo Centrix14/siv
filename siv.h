019005922623a56bd88b324e29774e39b7d4739e
Centrix14 <artiom.zvyagin@yandex.ru>
2020-09-18 20:31:27 +0300

    small header for siv.c

---
+++ siv.h
@@ -0,0 +1,9 @@
+#ifndef __SIMPLE_IMAGE_VIEWER_HEADER_FILE__
+#define __SIMPLE_IMAGE_VIEWER_HEADER_FILE__
+
+void zoom_inc_bttn_click(GtkWidget *bttn, gpointer data);
+void zoom_dec_bttn_click(GtkWidget *bttn, gpointer data);
+void next_bttn_click(GtkWidget *bttn, GtkWidget *window);
+void prev_bttn_click(GtkWidget *bttn, GtkWidget *window);
+
+#endif
