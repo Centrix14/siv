11eb8feeeff9583e0e0c02210103ad21f82fd0fa
Centrix14 <artiom.zvyagin@yandex.ru>
2020-09-20 13:45:48 +0300

    added send

--- siv.h
+++ siv.h
@@ -5,7 +5,11 @@ void zoom_inc_bttn_click(GtkWidget *bttn, gpointer data);
 void zoom_dec_bttn_click(GtkWidget *bttn, gpointer data);
 void next_bttn_click(GtkWidget *bttn, GtkWidget *window);
 void prev_bttn_click(GtkWidget *bttn, GtkWidget *window);
+void shell_bttn_click(GtkWidget *bttn, GtkWidget *parent);
 
 void key_press(GtkWidget *window, GdkEventKey *event, gpointer data);
+
+void send_bttn_click(GtkWidget *bttn, GtkWidget *entry);
+void set_window(GtkWidget *new_window);
 
 #endif
